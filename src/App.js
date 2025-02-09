1050a30b883887bb0ca1099a5fa40fad71f434a3
musiked7 <95246508+musiked7@users.noreply.github.com>
2025-02-09 14:26:33 -0600

    Mapped two projects in my App component's return statement

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import projects from "./projects.json";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+        {projects.map((project) => (
+          <div key={project.name}>
+            <h2>{project.name}</h2>
+            <p>{project.description}</p>
+            <a href={project.link}>View Project</a>
+            <img src={project.image} alt={project.name} />
+          </div>
+        ))}
+        </div>
       </header>
     </div>
   );
